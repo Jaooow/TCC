757a29a577b812cbab749db242dc68e1e40ce49a
Jaooow <joaopedro_cc@hotmail.com>
2021-11-25 11:17:11 -0300

    Conexação BD

--- conexao.php
+++ conexao.php
@@ -1,9 +1,9 @@
 <?php
 
-	$host = "localhost";
-	$bd = "tcc";
-	$usuario = "root";
-	$senha = "usbw";
+	$host = "us-cdbr-east-04.cleardb.com";
+	$bd = "heroku_dc4bc4f63154b1a";
+	$usuario = "b16ab79e4f5db0";
+	$senha = "c02b32de";
 
 	$con = mysqli_connect($host,$usuario,$senha,$bd) or die("Erro ao abrir a conexão com o banco de dados.");
 
